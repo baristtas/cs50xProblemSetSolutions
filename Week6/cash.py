4a2fa8023f3a09e04c4e2fc574a345bccdb6a166
baris <baristtas@gmail.com>
2020-12-16 14:47:30 +0300

    cash.py bugfix

--- Week6/cash.py
+++ Week6/cash.py
@@ -4,22 +4,22 @@ owe = -1
 # 0.25 0.10 0.05 0.01
 if (owe < 0):
     owe = get_float("How much we owe to you baby?: ")
-
-quarterCount = owe / 0.25
+owe = owe * 100
+quarterCount = owe / 25
 quarterCount = int(quarterCount)
-owe = owe - (quarterCount * 0.25)
+owe = owe - (quarterCount * 25)
 
-dimeCount = owe / 0.10
+dimeCount = owe / 10
 dimeCount = int(dimeCount)
-owe = owe - (dimeCount * 0.10)
+owe = owe - (dimeCount * 10)
 
-nickelCount = owe / 0.05
+nickelCount = owe / 5
 nickelCount = int(nickelCount)
-owe = owe - (nickelCount * 0.05)
+owe = owe - (nickelCount * 5)
 
-pennyCount = owe / 0.01
+pennyCount = owe / 1
 pennyCount = int(pennyCount)
-owe = owe - (pennyCount * 0.01)
+owe = owe - (pennyCount * 1)
 
 totalIronCount = quarterCount + dimeCount + nickelCount + pennyCount
 
